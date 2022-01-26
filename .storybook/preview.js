0cdff443bfe53c547e0eaf70ff7cd0ab940c1c0c
Yann Braga <yannbf@gmail.com>
2022-01-26 16:54:35 +0100

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
+setCompodocJson(docJson);
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
